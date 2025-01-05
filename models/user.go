18f2bcae0f79e5ce2963cd0577c6170a5f7dda2f
Arash Taher <arashThr@outlook.com>
2025-01-05 21:26:52 +0100

    12.3 User service

--- models/user.go
+++ models/user.go
@@ -1,7 +1,49 @@
 package models
 
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"golang.org/x/crypto/bcrypt"
+)
+
 type User struct {
 	ID           uint
 	Email        string
 	PasswordHash string
+}
+
+type UserService struct {
+	pool *pgxpool.Pool
+}
+
+func (us *UserService) Create(email, password string) (*User, error) {
+	email = strings.ToLower(email)
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("create user: %w", err)
+	}
+	passwordHash := string(hashedBytes)
+
+	row := us.pool.QueryRow(context.Background(), `
+		INSERT INTO users (email, password_hash) VALUES ($1, $2) RETURNING id
+	`, email, passwordHash)
+
+	user := User{
+		Email:        email,
+		PasswordHash: passwordHash,
+	}
+	err = row.Scan(&user.ID)
+
+	if err != nil {
+		return nil, fmt.Errorf("create user: %w", err)
+	}
+
+	return &user, nil
+}
+
+func (us *UserService) Update(user *User) error {
+	return nil
 }
