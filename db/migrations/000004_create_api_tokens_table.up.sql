0324dd0438518793d5a4f7e7894f9c831f8e2a2b
Arash Taher <arashThr@outlook.com>
2025-02-06 20:03:25 +0100

    Update table

--- db/migrations/000004_create_api_tokens_table.up.sql
+++ db/migrations/000004_create_api_tokens_table.up.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS api_tokens (
     id SERIAL PRIMARY KEY,
     user_id INT UNIQUE REFERENCES users (id) ON DELETE CASCADE,
-    token_hash TEXT UNIQUE NOT NULL,
-    expires_at TIMESTAMPTZ NOT NULL
+    token_hash TEXT UNIQUE NOT NULL
 );
